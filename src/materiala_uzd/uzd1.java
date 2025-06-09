4201caf81e37f8258e96d44002794f12db6081c1
BaxBas <cabelebaiba@gmail.com>
2025-06-09 19:50:21 +0300

    izlaboti uzdevumu nosaukumi un 1.uzd papildināts ar paskaidrojošiem komentāriem

--- src/materiala_uzd/uzd1.java
+++ src/materiala_uzd/uzd1.java
@@ -5,16 +5,34 @@ import java.util.ArrayList;
 public class uzd1 {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> skaitli = new ArrayList<>();
+		// 1.Izveido sarakstu ar 5 produktiem
+        ArrayList<String> produkti = new ArrayList<>();
+        produkti.add("Maize");
+        produkti.add("Piens");
+        produkti.add("Siers");
+        produkti.add("Tomāti");
+        produkti.add("Mellenes");
 
-		for (int i=1;i<=10;i++) {
-			skaitli.add(i);
-		}
+        System.out.println("Sākotnējais saraksts:");
+        System.out.println(produkti);
 
-        System.out.println("Skaitļi lielāki par 5:");
-        for (int s:skaitli) {
-        	if (s>5)
-        		System.out.println(s);
-        }
+        // 2.Izņem pirmo produktu
+        produkti.remove(0); // 1.produkta indekss ir 0, izņem "Maize"
+
+        System.out.println("\nPēc pirmā produkta izņemšanas:");
+        System.out.println(produkti);
+
+        // 3.Izdrukā produktu ar indeksu 3
+        System.out.println("\nProdukts ar indeksu 3:");
+        System.out.println(produkti.get(3));
+
+        // 4.Nomaini produktu ar indeksu 2 uz “Zemenes”
+        produkti.set(2,"Zemenes");
+
+        System.out.println("\nPēc aizvietošanas ar \"Zemenes\":");
+        System.out.println(produkti);
+
+        // 5.Izdrukā saraksta lielumu
+        System.out.println("\nSaraksta lielums: "+produkti.size());
 	}
 }
---
+++ src/materiala_uzd/uzd3.java
@@ -0,0 +1,21 @@
+package materiala_uzd;
+
+import java.util.ArrayList;
+
+public class uzd3 {
+
+	public static void main(String[] args) {
+		//Izveido sarakstu
+		ArrayList<Integer> skaitli = new ArrayList<>();
+		//Piepilda to ar skaitļiem 1-10
+		for (int i=1;i<=10;i++) {
+			skaitli.add(i);
+		}
+		//Izdrukā skaitļus >5 ar pārbaudi, izmantojot for-each cikla īpašības
+        System.out.println("Skaitļi lielāki par 5:");
+        for (int s:skaitli) {
+        	if (s>5)
+        		System.out.println(s);
+        }
+	}
+}
