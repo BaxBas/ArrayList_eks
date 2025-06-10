467320e936583e19210ab634884e517389f0dc7f
BaxBas <cabelebaiba@gmail.com>
2025-06-10 17:51:23 +0300

    Metode jautājumu vērtību nolašīšanai pabeigta. 1. jaut informācijas izvade uz ekrāna un atbilžu pārbaude izveidota

--- src/MainDala.java
+++ src/MainDala.java
@@ -11,18 +11,40 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class MainDala {
+	//globalie mainiigie
+	public static String uzdNos;
+	public static String jautajums;
+	public static ArrayList<String> atbVar = new ArrayList<>();
+	public static ArrayList<Integer> atbildes = new ArrayList<>();
+	
 	static void apskatit(String teksts) {
 		try {
 	        FileReader fr = new FileReader(teksts);
 	        BufferedReader br = new BufferedReader(fr);
 
-	        String pirmāRinda = br.readLine();
-	        String otrāRinda = br.readLine();
+	        uzdNos = br.readLine();
+	        jautajums = br.readLine();
+	        atbVar.clear();
+	        for(int i=0;i<4;i++) {
+	        	atbVar.add(br.readLine());
+	        }
+	        String pareizie = br.readLine();
 	        br.close();
+	        
+	        String[] parts = pareizie.split(",");
+
+	        for (String s : parts) {
+	            atbildes.add(Integer.parseInt(s.trim())-1);
+	        }
 
 	        String nolasitais = "";
-	        nolasitais += "1. rinda: " + pirmāRinda + "\n";
-	        nolasitais += "2. rinda: " + otrāRinda;
+	        nolasitais += uzdNos;
+	        nolasitais += "\n"+jautajums+"\n";
+	        for (String variants:atbVar) {
+	        	nolasitais += variants+"\n";
+	        }
+	        nolasitais += atbildes;
+	        
 	        JOptionPane.showMessageDialog(null, nolasitais, "Faila saturs", JOptionPane.INFORMATION_MESSAGE);
 	        
 	    } catch (IOException e) {
@@ -35,37 +57,30 @@ public class MainDala {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-		JLabel questionLabel = new JLabel("Kura ir pareizā atbilde?");
-		panel.add(questionLabel);
+		JLabel jautTeik = new JLabel(jautajums);
+		panel.add(jautTeik);
 
 		JCheckBox[] checkboxi = new JCheckBox[4];
-		String[] options = {"Opcija 1", "Opcija 2", "Opcija 3", "Opcija 4"};
-
-		for (int i=0; i<4; i++) {
-		    checkboxi[i] = new JCheckBox(options[i]);
+		for (int i=0; i<4;i++) {
+		    checkboxi[i] = new JCheckBox(atbVar.get(i));
 		    panel.add(checkboxi[i]);
 		}
 
-		//pareizaas atbildes
-		ArrayList<String> pareizi = new ArrayList<>();
-		pareizi.add("Opcija 1");
-		pareizi.add("Opcija 3");
-
-		int rez = JOptionPane.showConfirmDialog(null, panel, "Jautājums 1", JOptionPane.OK_CANCEL_OPTION);
+		int rez = JOptionPane.showConfirmDialog(null, panel, uzdNos, JOptionPane.OK_CANCEL_OPTION);
 
 		if (rez == JOptionPane.OK_OPTION) {
-		    ArrayList<String> izvele = new ArrayList<>();
-		    for (JCheckBox cb : checkboxi) {
-		        if (cb.isSelected()) {
-		        	izvele.add(cb.getText());
+		    ArrayList<Integer> izvele = new ArrayList<>();
+		    for (int i=0;i<checkboxi.length;i++) {
+		        if (checkboxi[i].isSelected()) {
+		            izvele.add(i);
 		        }
 		    }
 
 		    //sakarto alfabeta seciba prieksh salidzinashanas (lai butu pec kartas, nevis kaa tika izveleti checkboxi)
-		    Collections.sort(pareizi);
+		    Collections.sort(atbildes);
 		    Collections.sort(izvele);
 
-		    if (izvele.equals(pareizi)) {
+		    if (izvele.equals(atbildes)) {
 		        JOptionPane.showMessageDialog(null, "Pareizi!");
 		    } else {
 		        JOptionPane.showMessageDialog(null, "Nepareizi!");
